41bcd393b361ab1484f01feb9b75611723caeb7c
Matt Jewell <matt.jewell@paulina24.de>
2025-11-04 15:03:47 +0100

    2017: day11

--- solutions/2017/day11/part2.ts
+++ solutions/2017/day11/part2.ts
@@ -1,7 +1,46 @@
+import assert from "assert";
+
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const steps = input.trim().split(",") as Direction[];
+  const pos: Position = { q: 0, r: 0, s: 0 };
+  let furthest = -Infinity;
 
-  // Your solution here
+  for (const dir of steps) {
+    step(pos, dir);
+    furthest = Math.max(distance(pos), furthest);
+  }
 
-  return 0;
+  return furthest;
+}
+
+interface Position {
+  q: number;
+  r: number;
+  s: number;
+}
+
+const directions = {
+  n: [0, -1, 1],
+  ne: [1, -1, 0],
+  se: [1, 0, -1],
+  s: [0, 1, -1],
+  sw: [-1, 1, 0],
+  nw: [-1, 0, 1],
+} as const;
+
+type Direction = keyof typeof directions;
+
+// mutates and returns. fight me.
+function step(pos: Position, dir: string): Position {
+  const direction = directions[dir as Direction];
+  assert(direction, `Direction ${dir} is invalid`);
+  const [q, r, s] = direction;
+  pos.q += q;
+  pos.r += r;
+  pos.s += s;
+  return pos;
+}
+
+function distance(pos: Position) {
+  return (Math.abs(pos.q) + Math.abs(pos.r) + Math.abs(pos.s)) / 2;
 }
