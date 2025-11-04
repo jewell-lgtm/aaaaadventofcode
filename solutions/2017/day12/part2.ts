314b5ef2a4d7a3277b9dd28f42f0d8b81fff60db
Matt Jewell <matt.jewell@paulina24.de>
2025-11-04 15:42:23 +0100

    2017: day12

--- solutions/2017/day12/part2.ts
+++ solutions/2017/day12/part2.ts
@@ -1,7 +1,72 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const lines = input
+    .trim()
+    .split("\n")
+    .map((it) => parseLine(it));
 
-  // Your solution here
+  const pipes = createPipes();
+  for (const [from, allTo] of lines) {
+    for (const to of allTo) {
+      pipes.link(from, to);
+    }
+  }
 
-  return 0;
+  let groups = 0;
+  const notFound = new Set(pipes.pipes());
+
+  while (notFound.size) {
+    const first = notFound.values().next().value!;
+    const connected = pipes.connected(first);
+
+    for (const pipe of connected) {
+      notFound.delete(pipe);
+    }
+
+    groups++;
+  }
+
+  console.log(notFound.size);
+
+  return groups;
+}
+
+function parseLine(input: string): [string, string[]] {
+  const [from, allTo] = input.split(" <-> ");
+  return [from, allTo.split(",").map((it) => it.trim())];
+}
+
+function createPipes() {
+  const pipes = new Map<string, Set<string>>();
+  return {
+    pipes() {
+      return pipes.keys();
+    },
+    link(a: string, b: string) {
+      if (!pipes.has(a)) {
+        pipes.set(a, new Set());
+      }
+      if (!pipes.has(b)) {
+        pipes.set(b, new Set());
+      }
+      pipes.get(a)!.add(b);
+      pipes.get(b)!.add(a);
+    },
+    connected(to: string) {
+      const toSearch: string[] = [to];
+      const result = new Set<string>([to]);
+
+      while (toSearch.length > 0) {
+        const pipe = toSearch.pop()!!;
+        const connections = pipes.get(pipe) ?? new Set();
+        for (const connection of connections) {
+          if (!result.has(connection)) {
+            toSearch.push(connection);
+          }
+          result.add(connection);
+        }
+      }
+
+      return result;
+    },
+  };
 }
