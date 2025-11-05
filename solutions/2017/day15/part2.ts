12fb78f9a919b342340e2074ebc269eb28f86840
Matt Jewell <matt.jewell@paulina24.de>
2025-11-05 10:04:37 +0100

    2017: day15

--- solutions/2017/day15/part2.ts
+++ solutions/2017/day15/part2.ts
@@ -1,7 +1,35 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const [first, second] = input.trim().split("\n");
+  const aStart = parseInt(first.split(" ")[4], 10);
+  const bStart = parseInt(second.split(" ")[4], 10);
+
+  const genA = generator(16807, aStart, 4);
+  const genB = generator(48271, bStart, 8);
+
+  let matches = 0;
+
+  for (let i = 0; i < 5000000; i++) {
+    if (genA.next() === genB.next()) {
+      matches++;
+    }
+  }
 
   // Your solution here
 
-  return 0;
+  return matches;
+}
+
+function generator(factor: number, start: number, multiple: number) {
+  const mod = 2147483647;
+  let prev = start;
+  return {
+    next() {
+      prev = (prev * factor) % mod;
+      while (prev % multiple !== 0) {
+        prev = (prev * factor) % mod;
+      }
+      // just return the lowest 16 bits
+      return prev & 0xffff;
+    },
+  };
 }
