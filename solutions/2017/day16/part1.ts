c6fbae071c9161e5a1edbf17114ea5b6b1f093e2
Matt Jewell <matt.jewell@paulina24.de>
2025-11-05 10:17:28 +0100

    2017: day16

---
+++ solutions/2017/day16/part1.ts
@@ -0,0 +1,52 @@
+export function solve(input: string): number | string {
+  const instructions = input.trim().split(",");
+
+  const tape = "abcdefghijklmnop".split("");
+
+  for (const instruction of instructions) {
+    if (instruction[0] === "s") {
+      const amount = parseInt(instruction.slice(1), 10);
+      const last = tape.slice(0 - amount);
+      const first = tape.slice(0, 0 - amount);
+
+      for (let i = 0; i < last.length; i++) {
+        tape[i] = last[i];
+      }
+      for (let i = 0; i < first.length; i++) {
+        tape[i + last.length] = first[i];
+      }
+
+      continue;
+    }
+
+    if (instruction[0] === "x") {
+      const [first, second] = instruction
+        .slice(1)
+        .split("/")
+        .map((it) => parseInt(it, 10));
+
+      const temp = tape[first];
+      tape[first] = tape[second];
+      tape[second] = temp;
+
+      continue;
+    }
+
+    if (instruction[0] === "p") {
+      const [first, second] = instruction
+        .slice(1)
+        .split("/")
+        .map((it) => tape.indexOf(it));
+
+      const temp = tape[first];
+      tape[first] = tape[second];
+      tape[second] = temp;
+
+      continue;
+    }
+
+    throw new Error("TODO: " + instruction);
+  }
+
+  return tape.join("");
+}
