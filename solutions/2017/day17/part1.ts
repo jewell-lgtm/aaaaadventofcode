a9cfd4bf565954e129667701d434e3c679cc8de6
Matt Jewell <matt.jewell@paulina24.de>
2025-11-05 11:18:24 +0100

    2017: day17

--- solutions/2017/day17/part1.ts
+++ solutions/2017/day17/part1.ts
@@ -6,7 +6,13 @@ export function solve(input: string): number | string {
   const buffer = [0];
   let pos = 0;
 
+  let prevOneValue = undefined;
+
   for (let i = 1; i <= 2017; i++) {
+    if (buffer[1] != prevOneValue) {
+      // console.log(`Value in pos 1 is ${buffer[1]}`);
+      prevOneValue = buffer[1];
+    }
     pos = (pos + stepSize) % buffer.length;
     const before = buffer.slice(0, pos + 1);
     const after = buffer.slice(pos + 1);
--- solutions/2017/day17/part2.ts
+++ solutions/2017/day17/part2.ts
@@ -1,7 +1,21 @@
+import assert from "assert";
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const stepSize = parseInt(input.trim(), 10);
 
-  // Your solution here
+  let result: number;
+  let pos = 0;
 
-  return 0;
+  for (let i = 1; i <= 50_000_000; i++) {
+    pos = (pos + stepSize) % i;
+
+    if (pos === 0) {
+      result = i;
+    }
+
+    pos = pos + 1;
+  }
+
+  assert(result!);
+
+  return result;
 }
