f6e70500444350705d2eb2797ab609b316dca978
Matt Jewell <matt.jewell@paulina24.de>
2025-12-02 06:31:21 +0100

    2025: day02

--- solutions/2025/day02/part1.ts
+++ solutions/2025/day02/part1.ts
@@ -1,7 +1,38 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const ranges = input
+    .trim()
+    .split("\n")
+    .flatMap((line) => line.split(","))
+    .map((r) => parseRange(r));
 
-  // Your solution here
+  let seen = [] as number[];
+  console.time("brute");
+  for (const [first, last] of ranges) {
+    for (let i = first; i <= last; i++) {
+      const string = i.toString();
+      if (string.length % 2 !== 0) continue;
+      if (firstHalf(string) == secondHalf(string)) {
+        seen.push(i);
+      }
+    }
+  }
 
-  return 0;
+  return sum(seen);
+}
+
+function parseRange(range: string) {
+  return range.split("-").map((it) => parseInt(it)) as [number, number];
+}
+
+function firstHalf(string: string) {
+  const half = Math.floor(string.length / 2);
+  return string.slice(0, half);
+}
+function secondHalf(string: string) {
+  const half = Math.ceil(string.length / 2);
+  return string.slice(half);
+}
+
+function sum(arr: number[]) {
+  return arr.reduce((a, b) => a + b, 0);
 }
