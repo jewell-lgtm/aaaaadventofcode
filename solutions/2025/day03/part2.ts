556de06f87470eccb89fb92edf74b214d4c6a873
Matt Jewell <matt.jewell@paulina24.de>
2025-12-03 13:09:52 +0100

    2025: day03

--- solutions/2025/day03/part2.ts
+++ solutions/2025/day03/part2.ts
@@ -1,7 +1,37 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const lines = input.trim().split("\n");
 
-  // Your solution here
+  const digitLength = 12;
+  let result = BigInt(0);
+  for (const line of lines) {
+    const positions = Array.from(
+      { length: digitLength },
+      (_, i) => line.length - digitLength + i,
+    );
+    function collect(at: number[]) {
+      const digits = at.map((it) => line[it]);
+      return BigInt(digits.join(""));
+    }
+    let biggest = collect(positions);
+    for (let posI = 0; posI < positions.length; posI++) {
+      const min = posI === 0 ? 0 : positions[posI - 1] + 1;
+      const max =
+        posI === positions.length - 1
+          ? line.length - 1
+          : positions[posI + 1] - 1;
+      // move the digit to the left, and slide to the right to see if we can improve it
+      positions[posI] = min;
+      biggest = collect(positions);
+      for (let j = min; j <= max; j++) {
+        const guess = positions.map((p, idx) => (idx === posI ? j : p));
+        if (collect(guess) > biggest) {
+          biggest = collect(guess);
+          positions[posI] = j;
+        }
+      }
+    }
+    result += biggest;
+  }
 
-  return 0;
+  return result.toString();
 }
