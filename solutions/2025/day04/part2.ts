de42289905eed3d6ae3f32ee8504f5092c46f073
Matt Jewell <matt.jewell@paulina24.de>
2025-12-04 06:30:07 +0100

    2025: day04

--- solutions/2025/day04/part2.ts
+++ solutions/2025/day04/part2.ts
@@ -1,7 +1,54 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const grid = input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(""));
 
-  // Your solution here
+  let result = 0;
+  let active = true;
 
-  return 0;
+  while (active) {
+    let generation = 0;
+    for (let y = 0; y < grid.length; y++) {
+      const line = grid[y];
+      for (let x = 0; x < line.length; x++) {
+        const char = line[x];
+        if (char === "@") {
+          let count = 0;
+          for (const neighbor of neighbors(y, x)) {
+            if (neighbor === "@") {
+              count += 1;
+            }
+          }
+          if (count < 4) {
+            generation += 1;
+            grid[y][x] = ".";
+          }
+        }
+      }
+    }
+    result += generation;
+    active = generation > 0;
+  }
+
+  function neighbors(y: number, x: number): string[] {
+    return [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ]
+      .map(([dy, dx]) => {
+        if (grid[y + dy] && grid[y + dy][x + dx]) {
+          return grid[y + dy][x + dx];
+        }
+      })
+      .filter((it) => it != null);
+  }
+
+  return result;
 }
