2583a857cb0f254ce20e395e65c46c3cb83ea741
Matt Jewell <matt.jewell@paulina24.de>
2025-12-06 07:49:51 +0100

    2025: day06

--- solutions/2025/day06/part2.ts
+++ solutions/2025/day06/part2.ts
@@ -1,7 +1,41 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const grid = removeBlankLines(input)
+    .split("\n")
+    .map((it) => it.split("").reverse());
+  console.log(grid);
+
+  let result = 0n;
+  const sum: bigint[] = [];
+  for (let i = 0; i < grid[0].length; i++) {
+    const nums: string[] = [];
+    for (let j = 0; j < grid.length; j++) {
+      if (j === grid.length - 1) {
+        const num = nums.join("");
+        if (Number(num) > 0) {
+          sum.push(BigInt(nums.join("")));
+        }
+        if (grid[j][i] === "+") {
+          result += sum.reduce((a, b) => a + b, 0n);
+          sum.length = 0;
+        } else if (grid[j][i] === "*") {
+          result += sum.reduce((a, b) => a * b, 1n);
+          sum.length = 0;
+        } else {
+          nums.length = 0;
+        }
+      }
+      nums.push(grid[j][i]);
+    }
+  }
 
   // Your solution here
 
-  return 0;
+  return result.toString();
+}
+
+function removeBlankLines(input: string): string {
+  return input
+    .split("\n")
+    .filter((line) => line.trim() !== "")
+    .join("\n");
 }
