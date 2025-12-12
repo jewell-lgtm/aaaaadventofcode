3abb0f2a6f7c83e5071c7b3b13110ce95f1a685a
Matt Jewell <m@mattjewell.co.uk>
2025-12-12 15:59:26 +0100

    2025: day11

--- solutions/2025/day11/part2.ts
+++ solutions/2025/day11/part2.ts
@@ -1,7 +1,38 @@
 export function solve(input: string): number | string {
-  const lines = input.trim().split('\n');
+  const lines = input
+    .trim()
+    .split("\n")
+    .map((it) => {
+      const [head, tail] = it.split(": ");
+      return [head, tail.split(" ")] as const;
+    });
 
-  // Your solution here
+  let result = 0;
 
-  return 0;
+  const nodes = new Map<string, string[]>(
+    lines.map(([node, edges]) => [node, edges]),
+  );
+
+  const memo = new Map<string, bigint>();
+  function pathsFrom(src: string, dest: string): bigint {
+    const key = [src, dest].join(".");
+    if (memo.has(key)) return memo.get(key)!!;
+    let result = 0n;
+
+    if (src === dest) return 1n;
+
+    for (const nextNode of nodes.get(src) ?? []) {
+      result += pathsFrom(nextNode, dest);
+    }
+
+    memo.set(key, result);
+    return result;
+  }
+
+  return (
+    pathsFrom("svr", "dac") *
+      pathsFrom("dac", "fft") *
+      pathsFrom("fft", "out") +
+    pathsFrom("svr", "fft") * pathsFrom("fft", "dac") * pathsFrom("dac", "out")
+  ).toString();
 }
